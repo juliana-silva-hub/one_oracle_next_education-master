b9183408c9056cb54207da02ca5f5fbf7c437669
Juliana Cristina da Silva <juliana2317.js@gmail.com>
2022-11-01 17:25:05 -0300

    aula de javaScript

---
+++ frontEnd_T3/javaScript/intro JS/5-atribuicaoVariaveis.js
@@ -0,0 +1,14 @@
+console.log("Trabalhando com atribuição de variáveis");
+const idade = 29;
+const nome = "Ricardo";
+const sobrenome = "Bugan"; 
+
+//console.log(nome + sobrenome);
+//console.log(nome, sobrenome);
+console.log(`Meu nome é ${nome} ${sobrenome}`); 
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+let contador = 0 
+contador =  contador + 1
