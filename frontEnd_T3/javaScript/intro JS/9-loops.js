2408f871611117d1171442224a606796b4e25a35
Juliana Cristina da Silva <juliana2317.js@gmail.com>
2022-11-10 16:01:31 -0300

    código novo

--- frontEnd_T3/javaScript/intro JS/9-loops.js
+++ frontEnd_T3/javaScript/intro JS/9-loops.js
@@ -9,19 +9,23 @@ const listaDeDestinos = new Array(
 const idadeComprador = 18;
 const estaAcompanhada = false;
 let temPassagemComprada = false;
-const destino = "Curitiba"
+const destino = "Salvador"
 
 console.log("\n Destinos possíveis: ");
 console.log(listaDeDestinos);
 
 const podeComprar =  idadeComprador >= 18 || estaAcompanhada == true;
 
-let contador = 0
+let contador = 0;
+let destinoExiste = false
 while(contador<3){
+
     if(listaDeDestinos[contador] == destino){
-        console.log("Destino existe") 
-    }else{ 
-        console.log("Destino não existe");
+        console.log("Destino existe");
+        destinoExiste = true;
+        break;
     }
     contador += 1;
-} 
+}        
+
+console.log("Destino existe: ", destinoExiste);
