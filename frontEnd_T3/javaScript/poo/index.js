efc2bc9d4fe73ebf3af3b272d90e28d05bf23ef2
Juliana Cristina da Silva <juliana2317.js@gmail.com>
2023-01-05 18:14:18 -0300

    pequena correção

--- frontEnd_T3/javaScript/poo/index.js
+++ frontEnd_T3/javaScript/poo/index.js
@@ -1,6 +1,6 @@
 import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
-import { ContaPoupanca, ContaPoupanca } from "./ContaPoupanca.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
 
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
@@ -8,7 +8,7 @@ const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
-const ContaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
 ContaPoupanca.sacar(10);
 
 
