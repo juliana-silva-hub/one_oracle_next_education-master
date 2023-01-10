aee7b6051a5f9711a988779429de00c9fa251aa8
Juliana Cristina da Silva <juliana2317.js@gmail.com>
2023-01-10 17:10:41 -0300

    atualização

--- frontEnd_T3/javaScript/poo/index.js
+++ frontEnd_T3/javaScript/poo/index.js
@@ -2,7 +2,6 @@ import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
 
-
 const cliente1 = new Cliente("Ricardo", 11122233309);
 
 const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
@@ -10,7 +9,7 @@ contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-ContaPoupanca.sacar(10);
+contaPoupanca.sacar(10);
 
 
 console.log(ContaPoupanca);
